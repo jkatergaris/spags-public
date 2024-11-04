c94d111b1c630ade8409ca81f422dc4f7dcf4f4e
Ioannis Katergaris <i.katergaris@infinitum.gr>
2024-11-04 19:30:48 +0200

    skata

---
+++ upgarde.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+new_ver=$1
+
+echo "new version: $new_ver"
+
+# Simulate release of the new docker images
+docker tag nginx:1.23.3 jkatergaris/nginx:$new_ver
+
+# Push new version to dockerhub
+docker push jkatergaris/nginx:$new_ver
+
+# Create temporary folder
+tmp_dir=$(mktemp -d)
+echo $tmp_dir
+
+# Clone GitHub repo
+git clone https://github.com/jkatergaris/spags-public.git $tmp_dir
+
+# Update image tag
+sed -i '' -e "s/jkatergaris\/nginx:.*/jkatergaris\/nginx:$new_ver/g" -f $tmp_dir/my-app/1-deployment.yaml
+
+# Commit and push
+cd $tmp_dir
+git add .
+git commit -m "Update image to $new_ver"
+git push
+
+# Optionally on build agents - remove folder
+rm -rf $tmp_dir
